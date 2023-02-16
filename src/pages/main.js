37233d281e7967e7d8c6d05ffb294e9b6fc66180
ITPATJIDR <ittipatjitrada@gmail.com>
2023-02-16 23:19:09 +0700

    add check new user 13

--- src/pages/main.js
+++ src/pages/main.js
@@ -33,7 +33,7 @@ export default function Main() {
     const res = await axios.post("https://line-hack-server.vercel.app/user/register",payload,{
       withCredentials: true,
       headers: {
-        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": "https://line-hack-client.vercel.app",
       }
     })
       console.log(res)
