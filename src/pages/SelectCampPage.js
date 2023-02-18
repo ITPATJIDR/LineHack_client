9ac9f8141f2b574939db4476fd9c83bfc239bb5d
ITPATJIDR <ittipatjitrada@gmail.com>
2023-02-18 16:33:53 +0700

    add image in select camp

--- src/pages/SelectCampPage.js
+++ src/pages/SelectCampPage.js
@@ -3,12 +3,16 @@ import { useLocation } from "react-router-dom"
 
 export default function SelectCampPage() {
 	const location = useLocation()
+	const {campImage ,campName ,campDescription,bookingPrice,campFacility,
+	campFacilityDescription, campFeeDescription, campLocation, campMode,
+	campPromotion, campPromotionRating, campRating
+	} = location.state.item
 
-	console.log(location.state)
+	console.log(location.state.item)
   return (
     <div>
 	<div>
-		image
+		<img src={campImage} alt={campName} style={{width:"100%",height:300,borderBottomRadius:10}}/>
 	</div>
 	<div>
 		body	
