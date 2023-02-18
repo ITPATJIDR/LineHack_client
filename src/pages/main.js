c0f2c7084560389f4d4b1c955487c765504b9286
ITPAT <itpat@ITPATs-MacBook-Air.local>
2023-02-18 09:27:23 +0700

    test register2

--- src/pages/main.js
+++ src/pages/main.js
@@ -64,7 +64,7 @@ export default function Main() {
 
   return (
     <div style={{backgroundColor:"#1CC09E",width:390,height:750}}>
-      {liff.isLoggedIn()
+      {liff.isLoggedIn
       ?<CampPage/>
       :<WaitingLogin/> 
       }
