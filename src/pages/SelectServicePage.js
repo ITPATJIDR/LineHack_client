a1429c125276fb144288d97cf5939ab656a37aa1
ITPATJIDR <ittipatjitrada@gmail.com>
2023-02-19 23:42:54 +0700

    add select service page

--- src/pages/SelectServicePage.js
+++ src/pages/SelectServicePage.js
@@ -7,7 +7,8 @@ export default function SelectServicePage() {
 	const location = useLocation()
 	const navigate = useNavigate()
 
-	console.log(location.state.item)
+
+	const { serviceImage, serviceDescription, serviceName, serviceLineContact, serviceDistance, serviceContact } = location.state.item
 
 	const handleBack = () => {
 		navigate("/")
@@ -17,100 +18,94 @@ export default function SelectServicePage() {
 	}
 
   return (
-	<div>test</div>
-	//   <div>
-	// 	  <div>
-	// 		  <img src={campImage} alt={campName} style={{ width: "100%", height: 300, borderBottomLeftRadius: 10, borderBottomRightRadius: 10 }} />
-	// 	  </div>
-	// 	  <div style={{padding:10}}>
-	// 		  <div>
-	// 			  <div style={{ display: "flex", justifyContent: "space-between", width: 390, alignItems: "center" }}>
-	// 				  <div style={{ fontSize: 20, fontWeight: "bold" }}>
-	// 					  <p>{campName}</p>
-	// 				  </div>
-	// 				  <div style={{marginRight:10 }}>
-	// 					  <ReactStars value={campRating} />
-	// 				  </div>
-	// 			  </div>
-	// 			  <div style={{display: 'flex',justifyContent: "space-between"}}>
-	// 				  <div style={{display:"flex",alignItems: "center"}}>
-	// 					  <img src={Spot} alt="spot" style={{ width: 15, height: 15 }} />
-	// 					  <p style={{marginLeft: 10}}>{campLocation}</p>
-	// 				  </div>
-	// 				  <div>
-	// 					  <p>{bookingPrice} / person</p>
-	// 				  </div>
-	// 			  </div>
-	// 			  <div>
-	// 				  <div style={{display:"flex",alignItems: "center"}}>
-	// 					  <img src={Map} alt="Map" style={{ width: 15, height: 15 }} />
-	// 					  <p style={{marginLeft: 10}}>{campName}</p>
-	// 				  </div>
-	// 			  </div>
-	// 		  </div>
-	// 		  <div style={{marginTop:10}}>
-	// 			  <div>
-	// 				  <div>
-	// 					  <p style={{fontWeight:"bold"}}>Description</p>
-	// 				  </div>
-	// 				  <div>
-	// 					  {campDescription}
-	// 				  </div>
-	// 			  </div>
-	// 			  <div style={{marginTop:10}}>
-	// 				  <div>
-	// 					  <p style={{fontWeight:"bold"}}>ค่าบริการเข้าชมอุทยานฯ</p>
-	// 				  </div>
-	// 				  <div>
-	// 					  {campFeeDescription}
-	// 				  </div>
-	// 			  </div>
-	// 		  </div>
-	// 		  <div style={{
-	// 			position: 'absolute',
-	// 			display:'flex',
-	// 			bottom: '0',
-	// 			justifyContent: "space-between",
-	// 			backgroundColor: "#F7F7F7",
-	// 			width:393,
-	// 			height:70,
-	// 			left:0,
-	// 			bottom:0,
-	// 			alignItems:"center",
-	// 			padding:20
-	// 		  }}>
-	// 			  <div>
-	// 				  <div onClick={() => handleBack()} style={{
-	// 					width:100,
-	// 					height:50,
-	// 					backgroundColor:"#FDAF17",
-	// 					justifyContent: 'center',
-	// 					display:"flex",
-	// 					alignItems:"center",
-	// 					borderRadius:10,
-	// 					fontWeight:"bold"
-	// 				  }}>
-	// 					Back
-	// 				  </div>
-	// 			  </div>
-	// 			  <div>
-	// 				  <div style={{
-	// 					width:100,
-	// 					height:50,
-	// 					backgroundColor:"#FDAF17",
-	// 					justifyContent: 'center',
-	// 					display:"flex",
-	// 					alignItems:"center",
-	// 					borderRadius:10,
-	// 					fontWeight:"bold" }}
-	// 					onClick={() => handleBookNow()}
-	// 				>
-	// 					  Book Now
-	// 				  </div>
-	// 			  </div>
-	// 		  </div>
-	// 	  </div>
-	//   </div>
-  )
+	  <div>
+		  <div>
+			  <img src={serviceImage} alt={serviceName} style={{ width: "100%", height: 300, borderBottomLeftRadius: 10, borderBottomRightRadius: 10 }} />
+		  </div>
+		  <div style={{ padding: 10 }}>
+			  <div>
+				  <div style={{ display: "flex", justifyContent: "space-between", width: 390, alignItems: "center" }}>
+					  <div style={{ fontSize: 20, fontWeight: "bold" }}>
+						  <p>{serviceName}</p>
+					  </div>
+				  </div>
+				  <div style={{ display: 'flex', justifyContent: "space-between" }}>
+					  <div style={{ display: "flex", alignItems: "center" }}>
+						  <img src={Spot} alt="spot" style={{ width: 15, height: 15 }} />
+						  <p style={{ marginLeft: 10 }}>{serviceContact}</p>
+					  </div>
+				  </div>
+				  <div>
+					  <div style={{ display: "flex", alignItems: "center" }}>
+						  <img src={Map} alt="Map" style={{ width: 15, height: 15 }} />
+						  <p style={{ marginLeft: 10 }}>{serviceLineContact}</p>
+					  </div>
+				  </div>
+			  </div>
+			  <div style={{ marginTop: 10 }}>
+				  <div>
+					  <div>
+						  <p style={{ fontWeight: "bold" }}>Description</p>
+					  </div>
+					  <div>
+						  {serviceDescription}
+					  </div>
+				  </div>
+				  <div style={{marginTop:10}}>
+					  <div>
+						  <p style={{fontWeight:"bold"}}>ค่าบริการเข้าชมอุทยานฯ</p>
+					  </div>
+					  <div>
+						  {serviceDistance}
+					  </div>
+				  </div>
+			  </div>
+			  <div style={{
+				  position: 'absolute',
+				  display: 'flex',
+				  bottom: '0',
+				  justifyContent: "space-between",
+				  backgroundColor: "#F7F7F7",
+				  width: 393,
+				  height: 70,
+				  left: 0,
+				  bottom: 0,
+				  alignItems: "center",
+				  padding: 20
+			  }}>
+				  <div>
+					  <div onClick={() => handleBack()} style={{
+						  width: 100,
+						  height: 50,
+						  backgroundColor: "#FDAF17",
+						  justifyContent: 'center',
+						  display: "flex",
+						  alignItems: "center",
+						  borderRadius: 10,
+						  fontWeight: "bold"
+					  }}>
+						  Back
+					  </div>
+				  </div>
+				  <div>
+					  <div style={{
+						  width: 100,
+						  height: 50,
+						  backgroundColor: "#FDAF17",
+						  justifyContent: 'center',
+						  display: "flex",
+						  alignItems: "center",
+						  borderRadius: 10,
+						  fontWeight: "bold"
+					  }}
+						  onClick={() => handleBookNow()}
+					  >
+						  Book Now
+					  </div>
+				  </div>
+			  </div>
+		  </div>
+	  </div>
+	)
 
 }
