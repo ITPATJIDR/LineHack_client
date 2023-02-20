e0be27ba5a0523862d15b951b782c97b76244841
ITPATJIDR <ittipatjitrada@gmail.com>
2023-02-20 20:25:59 +0700

    test

--- src/pages/CampLoctionPage.js
+++ src/pages/CampLoctionPage.js
@@ -18,12 +18,39 @@ export default function CampLoctionPage() {
 			<Header alignItems={""} pageService={"Location"} pageMain={"Camp"}/>
 			<Body alignItems={""} justifyContent={""} display={""}>
 				<div>
-
+					<div>
+						สิ่งอำนวนความสะดวก
+					</div>
+					<div>
+						<input type="checkbox"/>
+						<p>อุปกรณ์ให้เช่า</p>
+					</div>
+					<div>
+						<input type="checkbox"/>
+						<p>Wifi</p>
+					</div>
+					<div>
+						<input type="checkbox"/>
+						<p>สัญญาญโทรศัพท์</p>
+					</div>
+					<div>
+						<input type="checkbox"/>
+						<p>ไฟฟ้า</p>
+					</div>
+					<div>
+						<input type="checkbox"/>
+						<p>เหมาะสำหรับ</p>
+					</div>
+					<div>
+						<input type="checkbox"/>
+						<p>ห้องน้ำ</p>
+					</div>
+					<div>
+						<input type="checkbox"/>
+						<p>อุปกรณ์ให้เช่า</p>
+					</div>
 				</div>	
 			</Body>
-			<div onClick={() => handlePayment()}>
-				Paynow	
-			</div>
 		</div>
 	)
 }
