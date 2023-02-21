8e15461d2740826a76043d118439b20b03a4ff77
ITPATJIDR <ittipatjitrada@gmail.com>
2023-02-21 18:39:43 +0700

    qr code

--- src/pages/PaymentSuccessPage.js
+++ src/pages/PaymentSuccessPage.js
@@ -29,7 +29,11 @@ export default function PaymentPage() {
         <p>Payment</p>
       </div>
       <div style={{display: "flex", justifyContent:"center",height:300,alignItems:'center',zIndex:1}}>
-
+        <div style={{width:250,height:300,display: "flex",justifyContent:"center",marginTop:50,background:"white",flexDirection:"column",justifyContent:"center",borderRadius:10,alignItems:"center",padding:10}}>
+            <div>
+              Payment Completed
+            </div>
+        </div>
       </div>
       <div style={{display: "flex",justifyContent: "center"}}>
         <div style={{position:"absolute",bottom:190,display: "flex",justifyContent:"center",width:100,height:50,alignItems: "center",borderRadius:10,fontWeight:"bold",background:"white"}}>
