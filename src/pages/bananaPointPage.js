6784326aeb3ed5f71581715afb633ac28e36a48f
ITPATJIDR <ittipatjitrada@gmail.com>
2023-02-23 23:52:13 +0700

    add fetch all shop

--- src/pages/bananaPointPage.js
+++ src/pages/bananaPointPage.js
@@ -88,6 +88,32 @@ export default function BananaPointPage() {
           <div style={{ paddingTop: 10, paddingLeft: 20 }}>
             <p style={{ fontSize: 20, fontWeight: "bold" }}>Banana Reward</p>
           </div>
+          <div>
+									{allShop.length > 0
+										? allShop.map((item, index) => {
+                      console.log(item)
+											return (
+												// <div key={index} style={{ width: seeMore === true && page === "Service" ? 130 : 400
+												// , height: 220, borderWidth: 1, borderRadius: 10, marginRight: 20,
+												// marginBottom: seeMore === true && page === "Service" ? 10 :0,
+												// overflow:"hidden",textOverflow:"ellipsis" }} 
+												// onClick={() => handleClick(item)}
+												// >
+												// 	<img src={item.serviceImage} alt={item.serviceName} style={{width:"100%",height:130,borderTopRightRadius:10,borderTopLeftRadius:10}}/>
+												// 	<div style={{padding:10}}>
+												// 		<p style={{fontWeight:'bold'}}>{item.serviceName}</p>
+												// 		<p style={{lineBreak:"anywhere"}}>{item.serviceDescription}</p>
+												// 	</div>
+												// </div>
+                        <div></div>
+
+											)
+										})
+										: <div>
+											<p>Please Booking First</p>
+										</div>
+										}
+          </div>
         </div>
       </Body>
       <Footer />
