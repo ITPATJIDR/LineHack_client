0586e710edd73988d241ec7193351fcda8fcdef1
ITPAT <itpat@ITPATs-MacBook-Air.local>
2023-02-26 05:11:26 +0700

    test

--- src/pages/SelectCampPage.js
+++ src/pages/SelectCampPage.js
@@ -80,9 +80,9 @@ export default function SelectCampPage() {
 		  <div>
 			  <img src={campImage} alt={campName} style={{ width: "100%", height: 300, borderBottomLeftRadius: 10, borderBottomRightRadius: 10 }} />
 		  </div>
-			  <div>
-				{renderCampMode(campMode)}
-			  </div>
+		  <div>
+			  {renderCampMode(campMode)}
+		  </div>
 		  <div style={{padding:10}}>
 			  <div>
 				  <div style={{ display: "flex", justifyContent: "space-between", width: 390, alignItems: "center" }}>
