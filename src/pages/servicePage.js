0df4b564598d1d5bced058ca8aaf6c0bfff578d0
ITPAT <itpat@ITPATs-MacBook-Air.local>
2023-02-25 09:28:35 +0700

    tes

--- src/pages/servicePage.js
+++ src/pages/servicePage.js
@@ -166,22 +166,26 @@ export default function ServicePage() {
 									}}>
 									{allService.data?.Booking.length > 0
 										? allService.data.Booking.map((item, index) => {
-											console.log(item)
-											return (
-												<div></div>
-												// <div key={index} style={{ width: seeMore === true && page === "Service" ? 130 : 400 ,
-												// height: 220, borderWidth: 1, borderRadius: 10,marginRight: seeMore === true && page === "Service" ? 0 : 20,
-												// marginBottom: seeMore === true && page === "Service" ? 10 :0,
-												// overflow:"hidden",textOverflow:"ellipsis" }} 
-												// onClick={() => handleClick(item)}
-												// >
-												// 	<img src={item.serviceImage} alt={item.serviceName} style={{width:"100%",height:130,borderTopRightRadius:10,borderTopLeftRadius:10}}/>
-												// 	<div style={{padding:10}}>
-												// 		<p style={{fontWeight:'bold'}}>{item.serviceName}</p>
-												// 		<p style={{lineBreak:"anywhere"}}>{item.serviceDescription}</p>
-												// 	</div>
-												// </div>
-											)
+											item.map((item,index) => {
+												console.log(item)
+												return(
+													<div></div>
+												)
+											})
+											// return (
+											// 	// <div key={index} style={{ width: seeMore === true && page === "Service" ? 130 : 400 ,
+											// 	// height: 220, borderWidth: 1, borderRadius: 10,marginRight: seeMore === true && page === "Service" ? 0 : 20,
+											// 	// marginBottom: seeMore === true && page === "Service" ? 10 :0,
+											// 	// overflow:"hidden",textOverflow:"ellipsis" }} 
+											// 	// onClick={() => handleClick(item)}
+											// 	// >
+											// 	// 	<img src={item.serviceImage} alt={item.serviceName} style={{width:"100%",height:130,borderTopRightRadius:10,borderTopLeftRadius:10}}/>
+											// 	// 	<div style={{padding:10}}>
+											// 	// 		<p style={{fontWeight:'bold'}}>{item.serviceName}</p>
+											// 	// 		<p style={{lineBreak:"anywhere"}}>{item.serviceDescription}</p>
+											// 	// 	</div>
+											// 	// </div>
+											// )
 										})
 										: <div>
 											<p>Please Booking First</p>
