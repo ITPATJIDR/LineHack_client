100847b89aa32de071e9174809a9014ed0f773fe
ITPAT <itpat@ITPATs-MacBook-Air.local>
2023-02-26 10:54:30 +0700

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,18 +7,19 @@ export default function Footer() {
 	const [checkOpen, setCheckOpen ] = useState("")
 	const handleRedirect = (path,Name) =>{
 		navigate(path)
+		console.log(Name)
 		setCheckOpen(Name)
 	}
 
-	console.log(checkOpen)
-
   return (
     <div style={{width:"100%",height:60,position:"absolute",bottom:0,padding:5,display:"flex",justifyContent:"center",borderTopWidth:1,bottom:10,background:"#000000"}}> 
-		  <div onClick={() => handleRedirect("/","Camp") } style={{ width: "30%", marginRight: 10, height: 40, display: "flex", justifyContent: "center", alignContent: "center", marginTop: 5, display: "flex", alignItems: "center",
-		  backgroundColor: checkOpen === "Camp" ? "#FDAF17" : "#DFDFDF", 
-		  borderRadius: 5, color: "black" }}>
-		Camp
-	</div>
+		  <div onClick={() => handleRedirect("/", "Camp")} style={{
+			  width: "30%", marginRight: 10, height: 40, display: "flex", justifyContent: "center", alignContent: "center", marginTop: 5, display: "flex", alignItems: "center",
+			  backgroundColor: checkOpen === "Camp" ? "#FDAF17" : "#DFDFDF",
+			  borderRadius: 5, color: "black"
+		  }}>
+			  Camp
+		  </div>
 	<div onClick={() => handleRedirect("/bananaPoint","Point")} style={{ width: "30%",marginRight:10, height: 40, display: "flex", justifyContent: "center", alignContent: "center",marginTop:5,display:"flex",alignItems:"center",backgroundColor:checkOpen === "Point" ? "#FDAF17" : "#DFDFDF",borderRadius:5,color:"black"}}>
 		Point
 	</div>
