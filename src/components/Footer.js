3b5f6ffcb54b89a20bb9fe1a8f1356e310c29666
ITPAT <itpat@ITPATs-MacBook-Air.local>
2023-02-26 10:39:27 +0700

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,18 +11,18 @@ export default function Footer() {
 
   return (
     <div style={{width:"100%",height:50,position:"absolute",bottom:0,padding:5,display:"flex",justifyContent:"center",borderTopWidth:1,bottom:10,background:"#000000"}}> 
-		  <div onClick={() => { handleRedirect("/"), setCheckOpen("Camp") }} style={{ width: "30%", marginRight: 10, height: 40, display: "flex", justifyContent: "center", alignContent: "center", marginTop: 5, display: "flex", alignItems: "center", 
+		  <div onClick={() => (handleRedirect("/"), setCheckOpen("Camp")) } style={{ width: "30%", marginRight: 10, height: 40, display: "flex", justifyContent: "center", alignContent: "center", marginTop: 5, display: "flex", alignItems: "center", 
 		  backgroundColor: checkOpen === "Camp" ? "#FDAF17" : "#DFDFDF", 
 		  borderRadius: 5, color: "white" }}>
 		Camp
 	</div>
-	<div onClick={() => {handleRedirect("/bananaPoint"),setCheckOpen("Point")}} style={{ width: "30%",marginRight:10, height: 40, display: "flex", justifyContent: "center", alignContent: "center",marginTop:5,display:"flex",alignItems:"center",backgroundColor:checkOpen === "Point" ? "#FDAF17" : "#DFDFDF",borderRadius:5,color:"white"}}>
+	<div onClick={() => (handleRedirect("/bananaPoint"),setCheckOpen("Point"))} style={{ width: "30%",marginRight:10, height: 40, display: "flex", justifyContent: "center", alignContent: "center",marginTop:5,display:"flex",alignItems:"center",backgroundColor:checkOpen === "Point" ? "#FDAF17" : "#DFDFDF",borderRadius:5,color:"white"}}>
 		Point
 	</div>
-		  <div onClick={() => { handleRedirect("/booking"), setCheckOpen("Booking") }} style={{ width: "30%", marginRight: 10, height: 40, display: "flex", justifyContent: "center", alignContent: "center", marginTop: 5, display: "flex", alignItems: "center", backgroundColor: checkOpen === "Booking" ?"#FDAF17" : "#DFDFDF" , borderRadius: 5, color: "white" }}>
+		  <div onClick={() => ( handleRedirect("/booking"), setCheckOpen("Booking") )} style={{ width: "30%", marginRight: 10, height: 40, display: "flex", justifyContent: "center", alignContent: "center", marginTop: 5, display: "flex", alignItems: "center", backgroundColor: checkOpen === "Booking" ? "#FDAF17" : "#DFDFDF" , borderRadius: 5, color: "white" }}>
 		Booking	
 	</div>
-	<div onClick={() => {handleRedirect("/service"),setCheckOpen("Service")}} style={{ width: "30%", height: 40, display: "flex", justifyContent: "center", alignContent: "center",marginTop:5,display:"flex",alignItems:"center",backgroundColor:checkOpen === "Service" ?"#FDAF17" : "#DFDFDF",borderRadius:5,color:"white"}}>
+	<div onClick={() => (handleRedirect("/service"),setCheckOpen("Service"))} style={{ width: "30%", height: 40, display: "flex", justifyContent: "center", alignContent: "center",marginTop:5,display:"flex",alignItems:"center",backgroundColor:checkOpen === "Service" ? "#FDAF17" : "#DFDFDF",borderRadius:5,color:"white"}}>
 		Service
 	</div>
     </div>
